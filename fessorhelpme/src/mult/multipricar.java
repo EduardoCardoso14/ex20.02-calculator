269acca768c2ef361dc96fca1a3b4439dfafb181
962670 <962670@PITLAB2178806.senac.educ>
2024-02-20 07:44:12 -0300

    calculadora mais ou meno

---
+++ fessorhelpme/src/mult/multipricar.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mult;
+
+/**
+ *
+ * @author 962670
+ */
+public class multipricar {
+
+    public static int contax(int a, int b) {
+        int x = a * b;
+        return x;
+    }
+}
---
+++ fessorhelpme/src/soma/somar.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package soma;
+
+/**
+ *
+ * @author 962670
+ */
+public class somar {
+
+    public static int contax(int a, int b) {
+        int x = a + b;
+        return x;
+    }
+}
